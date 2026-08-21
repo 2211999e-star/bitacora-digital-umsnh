c201b7938b64574aded89872309eec5917baa502
2211999e <2211999e@umich.mx>
2026-08-21 08:51:18 -0600

    ui: rediseño login + pulido interfaz (v12)

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Cache básico para index + assets del modo simple.
  */
 
-const CACHE_VERSION = 'bitacora-simple-v11';
+const CACHE_VERSION = 'bitacora-simple-v12';
 
 const PRECACHE_URLS = [
   './',
